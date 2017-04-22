92d4540006fc455118c2645dbf2d02ad0b31bfc2
Richard <pxdp4r0x@gmail.com>
2017-04-22 07:43:00 -0700

    Change permissions

--- src/main/java/com/github/UnstablePancake/modules/commands/ConfigCommands.java
+++ src/main/java/com/github/UnstablePancake/modules/commands/ConfigCommands.java
@@ -1,6 +1,7 @@
 package com.github.UnstablePancake.modules.commands;
 
 import co.kaioru.distort.d4j.command.D4JCommandBuilder;
+import com.github.UnstablePancake.modules.Roles.RolePermissions;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.Status;
 
@@ -15,12 +16,14 @@ public class ConfigCommands extends Commands {
     public void setStatus(IDiscordClient client){
         reg.registerCommand(new D4JCommandBuilder("setstatus")
                 .build((args, msg) -> {
-                    if(msg.getAuthor().getID().equals("164909448043823104")){
+                    if(RolePermissions.isAdmin(msg)){
                         if(args.size() > 0) {
                             String status = msg.getContent().substring(10);
                             client.changeStatus(Status.game(status));
                         } else
                             client.changeStatus(Status.empty());
+                    } else {
+                        msg.getChannel().sendMessage(RolePermissions.noPermission());
                     }
                 }));
     }
--- src/main/java/com/github/UnstablePancake/modules/commands/RoleCommands.java
+++ src/main/java/com/github/UnstablePancake/modules/commands/RoleCommands.java
@@ -1,6 +1,7 @@
 package com.github.UnstablePancake.modules.commands;
 
 import co.kaioru.distort.d4j.command.D4JCommandBuilder;
+import com.github.UnstablePancake.modules.Roles.RolePermissions;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.util.EmbedBuilder;
@@ -20,34 +21,38 @@ public class RoleCommands extends Commands {
     public void addRole(){
         reg.registerCommand(new D4JCommandBuilder("addrole")
                 .build((args, msg) -> {
-                    if (msg.getAuthor().getID().equals("164909448043823104") && args.size() > 0){
-                        String id = args.get(0);
-                        String text = args.get(1);
-                        IRole role = null;
-                        List<IRole> roles = msg.getClient().getRoles();
+                    if (RolePermissions.isAdmin(msg)){
+                        if(args.size() > 0) {
+                            String id = args.get(0);
+                            String text = args.get(1);
+                            IRole role = null;
+                            List<IRole> roles = msg.getClient().getRoles();
 
-                        if(id.startsWith("<@")){
-                            id = "";
-                            for(int i = 0; i < args.get(0).length(); i++){
-                                if(Character.isDigit(args.get(0).charAt(i)))
-                                    id += args.get(0).charAt(i);
+                            if (id.startsWith("<@")) {
+                                id = "";
+                                for (int i = 0; i < args.get(0).length(); i++) {
+                                    if (Character.isDigit(args.get(0).charAt(i)))
+                                        id += args.get(0).charAt(i);
+                                }
+                            }
+
+                            for (int i = 0; i < roles.size(); i++) {
+                                if (roles.get(i).getName().equalsIgnoreCase(text))
+                                    role = roles.get(i);
+                            }
+
+                            if (role != null) {
+                                msg.getGuild().getUserByID(id).addRole(role);
+
+                                msg.getChannel().sendMessage(null, new EmbedBuilder()
+                                        .withColor(new Color(78, 185, 110))
+                                        .appendField(":hammer_pick: Role added", "**Role:** " + role.toString() + "\n**Target:** " + msg.getGuild().getUserByID(id).getName(), false)
+                                        .build(), false);
+                                msg.delete();
                             }
                         }
-
-                        for (int i = 0; i < roles.size(); i++) {
-                            if (roles.get(i).getName().equalsIgnoreCase(text))
-                                role = roles.get(i);
-                        }
-
-                        if (role != null){
-                            msg.getGuild().getUserByID(id).addRole(role);
-
-                            msg.getChannel().sendMessage(null, new EmbedBuilder()
-                                    .withColor(new Color(78, 185, 110))
-                                    .appendField(":hammer_pick: Role added", "**Role:** " + role.toString() + "\n**Target:** " + msg.getGuild().getUserByID(id).getName(), false)
-                                    .build(), false);
-                            msg.delete();
-                        }
+                    } else {
+                        msg.getChannel().sendMessage(RolePermissions.noPermission());
                     }
                 }));
     }
@@ -55,34 +60,38 @@ public class RoleCommands extends Commands {
     public void remRole(){
         reg.registerCommand(new D4JCommandBuilder("remrole")
                 .build((args, msg) -> {
-                    if (msg.getAuthor().getID().equals("164909448043823104") && args.size() > 0) {
-                        String id = args.get(0);
-                        String text = args.get(1);
-                        IRole role = null;
-                        List<IRole> roles = msg.getClient().getRoles();
+                    if(RolePermissions.isAdmin(msg)) {
+                        if (args.size() > 0) {
+                            String id = args.get(0);
+                            String text = args.get(1);
+                            IRole role = null;
+                            List<IRole> roles = msg.getClient().getRoles();
 
-                        if(id.startsWith("<@")){
-                            id = "";
-                            for(int i = 0; i < args.get(0).length(); i++){
-                                if(Character.isDigit(args.get(0).charAt(i)))
-                                    id += args.get(0).charAt(i);
+                            if (id.startsWith("<@")) {
+                                id = "";
+                                for (int i = 0; i < args.get(0).length(); i++) {
+                                    if (Character.isDigit(args.get(0).charAt(i)))
+                                        id += args.get(0).charAt(i);
+                                }
+                            }
+
+                            for (int i = 0; i < roles.size(); i++) {
+                                if (roles.get(i).getName().equalsIgnoreCase(text))
+                                    role = roles.get(i);
+                            }
+
+                            if (role != null) {
+                                msg.getGuild().getUserByID(id).removeRole(role);
+
+                                msg.getChannel().sendMessage(null, new EmbedBuilder()
+                                        .withColor(new Color(231, 76, 60))
+                                        .appendField(":hammer_pick: Role removed", "**Role:** " + role.toString() + "\n**Target:** " + msg.getGuild().getUserByID(id).getName(), false)
+                                        .build(), false);
+                                msg.delete();
                             }
                         }
-
-                        for (int i = 0; i < roles.size(); i++) {
-                            if (roles.get(i).getName().equalsIgnoreCase(text))
-                                role = roles.get(i);
-                        }
-
-                        if (role != null) {
-                            msg.getGuild().getUserByID(id).removeRole(role);
-
-                            msg.getChannel().sendMessage(null, new EmbedBuilder()
-                                    .withColor(new Color(231, 76, 60))
-                                    .appendField(":hammer_pick: Role removed", "**Role:** " + role.toString() + "\n**Target:** " + msg.getGuild().getUserByID(id).getName(), false)
-                                    .build(), false);
-                            msg.delete();
-                        }
+                    } else {
+                        msg.getChannel().sendMessage(RolePermissions.noPermission());
                     }
                 }));
     }
