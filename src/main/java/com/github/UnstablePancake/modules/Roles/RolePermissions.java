a1be9a56e02987f9550e50ba71253cabb272169e
Richard <pxdp4r0x@gmail.com>
2017-04-21 21:23:00 -0700

    Update permissions

--- src/main/java/com/github/UnstablePancake/modules/Roles/RolePermissions.java
+++ src/main/java/com/github/UnstablePancake/modules/Roles/RolePermissions.java
@@ -1,4 +1,5 @@
 package com.github.UnstablePancake.modules.Roles;
+import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.Permissions;
 import java.util.EnumSet;
 
@@ -9,4 +10,12 @@ public class RolePermissions {
     public static EnumSet<Permissions> regular = EnumSet.of(Permissions.CREATE_INVITE, Permissions.CHANGE_NICKNAME, Permissions.USE_EXTERNAL_EMOJIS, Permissions.READ_MESSAGES, Permissions.SEND_MESSAGES, Permissions.SEND_TTS_MESSAGES, Permissions.EMBED_LINKS, Permissions.ATTACH_FILES, Permissions.READ_MESSAGE_HISTORY, Permissions.MENTION_EVERYONE, Permissions.USE_EXTERNAL_EMOJIS, Permissions.ADD_REACTIONS, Permissions.VOICE_CONNECT, Permissions.VOICE_SPEAK, Permissions.VOICE_USE_VAD);
     public static EnumSet<Permissions> user = EnumSet.of(Permissions.CREATE_INVITE, Permissions.CHANGE_NICKNAME, Permissions.USE_EXTERNAL_EMOJIS, Permissions.READ_MESSAGES, Permissions.SEND_MESSAGES, Permissions.SEND_TTS_MESSAGES, Permissions.EMBED_LINKS, Permissions.ATTACH_FILES, Permissions.READ_MESSAGE_HISTORY, Permissions.MENTION_EVERYONE, Permissions.USE_EXTERNAL_EMOJIS, Permissions.ADD_REACTIONS, Permissions.VOICE_CONNECT, Permissions.VOICE_SPEAK, Permissions.VOICE_USE_VAD);
     public static EnumSet<Permissions> guest = EnumSet.of(Permissions.READ_MESSAGES, Permissions.SEND_MESSAGES, Permissions.EMBED_LINKS, Permissions.ATTACH_FILES, Permissions.USE_EXTERNAL_EMOJIS, Permissions.ADD_REACTIONS, Permissions.VOICE_CONNECT, Permissions.VOICE_SPEAK, Permissions.VOICE_USE_VAD);
+
+    public static boolean isAdmin(IMessage msg){
+        return msg.getAuthor().getPermissionsForGuild(msg.getGuild()).contains(Permissions.ADMINISTRATOR);
+    }
+
+    public static String noPermission(){
+        return ":no_entry_sign: You do not have permission for that command.";
+    }
 }
