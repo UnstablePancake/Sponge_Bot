13a3f0db3b7732c60adf36f76d4e7a811b4e4ba9
Richard <pxdp4r0x@gmail.com>
2017-04-21 18:05:00 -0700

    Clean up

--- src/main/java/com/github/UnstablePancake/modules/commands/GeneralCommands.java
+++ src/main/java/com/github/UnstablePancake/modules/commands/GeneralCommands.java
@@ -2,7 +2,6 @@ package com.github.UnstablePancake.modules.commands;
 
 import co.kaioru.distort.d4j.command.D4JCommandBuilder;
 import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.handle.obj.IUser;
 
 public class GeneralCommands extends Commands {
 
@@ -23,12 +22,12 @@ public class GeneralCommands extends Commands {
         reg.registerCommand(new D4JCommandBuilder("avatar")
                 .build((args, msg) -> {
                     String rawID = args.get(0);
-                    String ID = "";
+                    String id = "";
                     for(int i = 0; i < rawID.length(); i++){
                         if(Character.isDigit(rawID.charAt(i)))
-                            ID += rawID.charAt(i);
+                            id += rawID.charAt(i);
                     }
-                    msg.getChannel().sendMessage(msg.getClient().getUserByID(ID).getAvatarURL());
+                    msg.getChannel().sendMessage(msg.getClient().getUserByID(id).getAvatarURL());
         }));
     }
 }
