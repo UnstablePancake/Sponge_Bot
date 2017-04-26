4690af20e7878188b5d7ff287177e77fda2b7bfb
Richard <pxdp4r0x@gmail.com>
2017-04-26 15:56:05 -0700

    Add sync

--- src/main/java/com/github/UnstablePancake/modules/commands/ModeratorCommands.java
+++ src/main/java/com/github/UnstablePancake/modules/commands/ModeratorCommands.java
@@ -1,6 +1,8 @@
 package com.github.UnstablePancake.modules.commands;
 
 import co.kaioru.distort.d4j.command.D4JCommandBuilder;
+import com.github.UnstablePancake.bot.JSONHandler;
+import com.github.UnstablePancake.bot.Schedule;
 import com.github.UnstablePancake.bot.SpongeBot;
 import com.github.UnstablePancake.modules.Moderator;
 import com.github.UnstablePancake.modules.roles.RolePermissions;
@@ -10,15 +12,15 @@ public class ModeratorCommands extends Commands {
 
     private Moderator moderator = SpongeBot.moderator;
 
-    public ModeratorCommands(IDiscordClient client) {
+    public ModeratorCommands(IDiscordClient client){
         super(client);
         D4JModule.getListener().setPrefix("!");
         prune();
         setServerName();
         setServerDesc();
+        syncUserData();
         shutdown();
     }
-
 
     private void prune(){
         reg.registerCommand(new D4JCommandBuilder("prune")
@@ -66,12 +68,23 @@ public class ModeratorCommands extends Commands {
         }));
     }
 
+    private void syncUserData(){
+        reg.registerCommand(new D4JCommandBuilder("sync")
+                .build((args, msg) -> {
+                    if(RolePermissions.isAdmin(msg))
+                        JSONHandler.createJSON();
+        }));
+    }
+
     private void shutdown(){
         reg.registerCommand(new D4JCommandBuilder("shutdown")
                 .build((args, msg) -> {
-                    if(RolePermissions.isAdmin(msg))
+                    if(RolePermissions.isAdmin(msg)){
+                        JSONHandler.createJSON();
                         msg.getChannel().sendMessage("Shutting down...");
+                        Schedule.scheduler.shutdownNow();
                         msg.getClient().logout();
+                    }
         }));
     }
 }
