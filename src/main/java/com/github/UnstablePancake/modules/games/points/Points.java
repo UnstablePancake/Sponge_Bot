fa3c7607d00c441718f9fe2b211e031edf0532fb
Richard <pxdp4r0x@gmail.com>
2017-04-27 21:11:15 -0700

    Clean up

--- src/main/java/com/github/UnstablePancake/modules/games/points/Points.java
+++ src/main/java/com/github/UnstablePancake/modules/games/points/Points.java
@@ -7,11 +7,6 @@ import java.util.ArrayList;
 
 public class Points {
 
-    // remove variables
-    private static ArrayList<String> names = UserData.names;
-    private static ArrayList<String> ids = UserData.ids;
-    private static ArrayList<Integer> points = UserData.points;
-
     public static void addPoints(IUser user){
         int index = UserData.getIndex(user.getID());
         if(index >= 0){
@@ -20,13 +15,13 @@ public class Points {
     }
 
     private static int getCredits(int index){
-        return points.get(index);
+        return UserData.points.get(index);
     }
 
     public static int getCredits(IUser user){
         int credits = 0;
-        for(int i = 0; i < ids.size(); i++){
-            if(ids.get(i).equals(user.getID()))
+        for(int i = 0; i < UserData.ids.size(); i++){
+            if(UserData.ids.get(i).equals(user.getID()))
                 credits = getCredits(i);
         }
         return credits;
@@ -34,23 +29,32 @@ public class Points {
 
     public static int getCredits(String id){
         int credits = 0;
-        for(int i = 0; i < ids.size(); i++){
-            if(ids.get(i).equals(id))
+        for(int i = 0; i < UserData.ids.size(); i++){
+            if(UserData.ids.get(i).equals(id))
                 credits = getCredits(i);
         }
         return credits;
     }
 
+    public static int getTrophies(String id){
+        int t = 0;
+        for(int i = 0; i < UserData.ids.size(); i++){
+            if(UserData.ids.get(i).equals(id))
+                t = UserData.trophies.get(i);
+        }
+        return t;
+    }
+
     public static void addCredits(String id, int amount){
         int index = -1;
-        for(int i = 0; i < ids.size(); i++){
-            if(ids.get(i).equals(id)){
+        for(int i = 0; i < UserData.ids.size(); i++){
+            if(UserData.ids.get(i).equals(id)){
                 index = i;
             }
         }
         if(index != -1){
-            int initialCredits = points.get(index);
-            points.set(index, initialCredits + amount);
+            int initialCredits = UserData.points.get(index);
+            UserData.points.set(index, initialCredits + amount);
         }
     }
 
@@ -68,10 +72,10 @@ public class Points {
     public static String getTopPlayer(){
         String top = "Not found.";
         int high = 0;
-        for(int i = 0; i < points.size(); i++){
-            if(high < points.get(i)){
-                high = points.get(i);
-                top = names.get(i);
+        for(int i = 0; i < UserData.points.size(); i++){
+            if(high < UserData.points.get(i)){
+                high = UserData.points.get(i);
+                top = UserData.names.get(i);
             }
         }
         return top;
